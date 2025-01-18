7982287d7c91b4457287d877316e477124801c12
Jacob Budzis <jrbudzis@gmail.com>
2025-01-18 13:58:19 -0800

    Use raytracing to create black image

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use color::Color;
+use ray::Ray;
 use vec3::{Point3, Vec3};
 
 mod color;
@@ -6,23 +7,55 @@ mod ray;
 mod vec3;
 
 const ASPECT_RATIO: f32 = 16.0 / 9.0;
-const IMAGE_WIDTH: i32 = 256;
-const IMAGE_HEIGHT: i32 = 256;
+const IMAGE_WIDTH: i32 = 400;
+// TODO: Make sure this is always >= 1 with a terenary operator.
+const IMAGE_HEIGHT: i32 = (IMAGE_WIDTH as f32 / ASPECT_RATIO) as i32;
+
+const FOCAL_LENGTH: f32 = 1.0;
+const VIEWPORT_HEIGHT: f32 = 2.0;
+const VIEWPORT_WIDTH: f32 = VIEWPORT_HEIGHT * IMAGE_WIDTH as f32 / IMAGE_HEIGHT as f32;
+
 const MAX_COLOR: i32 = 256;
+
+fn color(_ray: Ray) -> Color {
+    Color::new(0.0, 0.0, 0.0)
+}
 
 fn main() {
     println!("P3");
     println!("{} {}", IMAGE_WIDTH, IMAGE_HEIGHT);
     println!("{}", MAX_COLOR);
+
+    let camera_center = Point3::new(0.0, 0.0, 0.0);
+
+    // Calculate the vectors across the viewport edges
+    let viewport_u = Vec3::new(VIEWPORT_WIDTH, 0.0, 0.0);
+    let viewport_v = Vec3::new(0.0, -VIEWPORT_HEIGHT, 0.0);
+
+    // Calculate the delta vectors between pixels
+    let pixel_delta_u = viewport_u / IMAGE_WIDTH as f32;
+    let pixel_delta_v = viewport_v / IMAGE_HEIGHT as f32;
+
+    // Calculate the location of the upper left pixel
+    let viewport_upper_left =
+        camera_center - Vec3::new(0.0, 0.0, FOCAL_LENGTH) - viewport_u / 2.0 - viewport_v / 2.0;
+    let pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
     for j in 0..IMAGE_HEIGHT {
         // eprintln!("Scanlines remaining: {}", IMAGE_HEIGHT - j);
         for i in 0..IMAGE_WIDTH {
-            let pixel = Color::new(
-                i as f32 / (IMAGE_WIDTH - 1) as f32,
-                j as f32 / (IMAGE_HEIGHT - 1) as f32,
-                0.0,
-            );
-            pixel.output();
+            let pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
+            let ray_direction = pixel_center - camera_center;
+
+            let ray = Ray::new(camera_center, ray_direction);
+            let color = color(ray);
+            color.output();
+            // let pixel = Color::new(
+            //     i as f32 / (IMAGE_WIDTH - 1) as f32,
+            //     j as f32 / (IMAGE_HEIGHT - 1) as f32,
+            //     0.0,
+            // );
+            // pixel.output();
         }
     }
     // eprintln!("done");
--- src/vec3.rs
+++ src/vec3.rs
@@ -1,4 +1,4 @@
-use std::ops::{Add, Mul};
+use std::ops::{Add, Div, Mul, Sub};
 
 #[derive(Clone, Copy)]
 pub struct Vec3 {
@@ -39,12 +39,50 @@ impl Add for Vec3 {
     }
 }
 
+impl Sub for Vec3 {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self {
+        Self {
+            e: [
+                self.e[0] - rhs.e[0],
+                self.e[1] - rhs.e[1],
+                self.e[2] - rhs.e[2],
+            ],
+        }
+    }
+}
+
 impl Mul<Vec3> for f32 {
     type Output = Vec3;
 
     fn mul(self, rhs: Vec3) -> Vec3 {
         Vec3 {
             e: [self * rhs.e[0], self * rhs.e[1], self * rhs.e[2]],
+        }
+    }
+}
+
+impl Mul<Vec3> for i32 {
+    type Output = Vec3;
+
+    fn mul(self, rhs: Vec3) -> Vec3 {
+        Vec3 {
+            e: [
+                self as f32 * rhs.e[0],
+                self as f32 * rhs.e[1],
+                self as f32 * rhs.e[2],
+            ],
+        }
+    }
+}
+
+impl Div<f32> for Vec3 {
+    type Output = Self;
+
+    fn div(self, rhs: f32) -> Self {
+        Vec3 {
+            e: [self.e[0] / rhs, self.e[1] / rhs, self.e[2] / rhs],
         }
     }
 }
