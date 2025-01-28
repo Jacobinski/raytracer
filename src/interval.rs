a1a29c4adc389991a6ab21f60649362cef0bf016
Jacob Budzis <jrbudzis@gmail.com>
2025-01-28 07:47:07 -0800

    cleanup: Remove unused methods and constants

--- src/interval.rs
+++ src/interval.rs
@@ -3,25 +3,10 @@ pub struct Interval {
     max: f64,
 }
 
-/// EMPTY represents an interval with nothing.
-pub const EMPTY: Interval = Interval {
-    min: f64::INFINITY,
-    max: f64::NEG_INFINITY,
-};
-/// UNIVERSE represents an interval with everything.
-pub const UNIVERSE: Interval = Interval {
-    min: f64::NEG_INFINITY,
-    max: f64::INFINITY,
-};
-
 impl Interval {
     pub fn new(min: f64, max: f64) -> Self {
         assert!(min <= max);
         Self { min, max }
-    }
-
-    pub fn size(&self) -> f64 {
-        self.max - self.min
     }
 
     pub fn surrounds(&self, val: f64) -> bool {
